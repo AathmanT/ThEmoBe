ea6ce5331345ae645e0ecb8a967372709dc5eeb4
T.Aathman <aathmant@gmail.com>
2020-12-11 14:56:15 +0530

    auto checkout colab-branch

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 #downloading and extracting face detector model
-cd ~/ThEmoBe_basic
+cd ~/ThEmoBe
+git checkout -b colab-branch origin/colab-branch
+
 
 wget https://s3-us-west-2.amazonaws.com/static.pyimagesearch.com/face-detection-opencv-deep-learning/deep-learning-face-detection.zip
 
@@ -10,25 +12,26 @@ unzip -f deep-learning-face-detection.zip
 
 
 #downloading GSOM code
-%cd ~/ThEmoBe_basic
+%cd ~/ThEmoBe
 
 git clone https://github.com/AathmanT/Parallel_GSOM_for_HAAP.git
-
-
+cd ~/ThEmoBe/Parallel_GSOM_for_HAAP/
+git checkout -b colab-branch origin/colab-branch
+cd ~/ThEmoBe/
 
 #downloading YOLO V3 model
-cd ~/ThEmoBe_basic
+
 
 git clone https://github.com/AathmanT/CVND_Exercises_2_2_YOLO.git
-
+cd ~/ThEmoBe/CVND_Exercises_2_2_YOLO/
+git checkout -b colab-branch origin/colab-branch
+cd ~/ThEmoBe/
 #wget -O yolov3.weights https://pjreddie.com/media/files/yolov3.weights
 cp "/content/drive/My Drive/yolov3.weights" "/content/ThEmoBe/"
 
 
 
 pip3 install -r requirements.txt
-
-sudo rm -rf /var/lib/mysql
 
 export DEBIAN_FRONTEND=noninteractive
 
