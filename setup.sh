7ad780afb5271aa4f70734e070d7a64c76213294
T.Aathman <aathmant@gmail.com>
2020-12-12 16:15:00 +0530

    fix: keras not predicting in celery
    print model summary
    
    
    hhfhj
    
    
    fix: keras not predicting in celery
    
    
    hjh

--- setup.sh
+++ setup.sh
@@ -1,31 +1,29 @@
 #!/usr/bin/env bash
 
 #downloading and extracting face detector model
-cd ~/ThEmoBe
-git checkout -b colab-branch origin/colab-branch
-
+cd /content/ThEmoBe
 
 wget https://s3-us-west-2.amazonaws.com/static.pyimagesearch.com/face-detection-opencv-deep-learning/deep-learning-face-detection.zip
 
-unzip -f deep-learning-face-detection.zip
+unzip deep-learning-face-detection.zip
 
 
 
 #downloading GSOM code
-%cd ~/ThEmoBe
+%cd /content/ThEmoBe
 
 git clone https://github.com/AathmanT/Parallel_GSOM_for_HAAP.git
-cd ~/ThEmoBe/Parallel_GSOM_for_HAAP/
+cd /content/ThEmoBe/Parallel_GSOM_for_HAAP/
 git checkout -b colab-branch origin/colab-branch
-cd ~/ThEmoBe/
+cd /content/ThEmoBe/
 
 #downloading YOLO V3 model
 
 
 git clone https://github.com/AathmanT/CVND_Exercises_2_2_YOLO.git
-cd ~/ThEmoBe/CVND_Exercises_2_2_YOLO/
+cd /content/ThEmoBe/CVND_Exercises_2_2_YOLO/
 git checkout -b colab-branch origin/colab-branch
-cd ~/ThEmoBe/
+cd /content/ThEmoBe/
 #wget -O yolov3.weights https://pjreddie.com/media/files/yolov3.weights
 cp "/content/drive/My Drive/yolov3.weights" "/content/ThEmoBe/"
 
